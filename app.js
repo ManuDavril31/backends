c6e949c5f44241d55f2f16ca73f8fffc9025b4bb
ManuDavril31 <manumar451@gmail.com>
2021-12-06 21:21:16 -0500

    Update 06/12/2021: 09:21:PM

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 /** @format */
 
-import express from "express";
-import mongoose from "mongoose";
-import Tickets from "./api/models/Tickets.js";
-
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
 const port = 5500;
 
@@ -16,32 +15,43 @@ mongoose.connect(mongoURL, {
 
 //middleware, ejecutar respuesta como archivo jSON
 app.use(express.json());
+app.use(cors());
 
-app.post("/api/tickets", (req, res) => {
-  let ticketsData = req.body;
-  let mongoTiket = [];
-  ticketsData.forEach((ticket) => {
-    mongoTiket.push({
-      nombre: ticket.nombre,
-      email: ticket.email,
-      telefono: ticket.telefono,
-      asunto: ticket.asunto,
-      mensaje: ticket.mensaje,
-    });
-    Tickets.create(mongoTiket, (err, records) => {
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        res.status(200).send(records);
-      }
-    });
-  });
-  res.send("You have posted something");
-});
+//Routes
+app.use("/api/tickets", require("./routes/ticket.route.js"));
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+// app.post("/api/tickets", (req, res) => {
+//   let ticketsData = req.body;
+//   let mongoTiket = [];
+//   ticketsData.forEach((ticket) => {
+//     mongoTiket.push({
+//       nombre: ticket.nombre,
+//       email: ticket.email,
+//       telefono: ticket.telefono,
+//       asunto: ticket.asunto,
+//       mensaje: ticket.mensaje,
+//     });
+//     Tickets.create(mongoTiket, (err, records) => {
+//       if (err) {
+//         res.status(500).send(err);
+//       } else {
+//         res.status(200).send(records);
+//       }
+//     });
+//   });
+//   // Tickets.create(req.body, (err, records) => {
+//   //   if (err) {
+//   //     res.status(500).send(err);
+//   //   } else {
+//   //     res.status(200).send(records);
+//   //   }
+//   // });
+//   res.send("You have posted something");
+// });
+
+// app.get("/", (req, res) => {
+//   res.json(res);
+// });
 
 //este listen admite dos parametros, el primero es el puerto, el segundo es una colback function
 app.listen(port, () => {
